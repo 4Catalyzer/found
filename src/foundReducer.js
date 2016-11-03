c554f47a9efd77fa403cdcbe3da35aeff7320dc9
Jimmy Jia <tesrin@gmail.com>
2016-11-03 02:10:24 -0400

    Clean up reducer code a bit

--- src/foundReducer.js
+++ src/foundReducer.js
@@ -1,20 +1,22 @@
 import ActionTypes from './ActionTypes';
 
 export default function foundReducer(state = null, action) {
-  switch (action.type) {
+  const { type, payload } = action;
+
+  switch (type) {
     case ActionTypes.UPDATE_MATCH:
       // For the initial match, set resolvedMatch too. There's no previous
       // result to keep rendered, plus this simplifies server rendering.
       return {
-        match: action.payload,
-        resolvedMatch: state ? state.resolvedMatch : action.payload,
+        match: payload,
+        resolvedMatch: state ? state.resolvedMatch : payload,
       };
     case ActionTypes.RESOLVE_MATCH:
       // It doesn't make sense to resolve a match if there wasn't already an
       // unresolved match.
       return state && {
         match: state.match,
-        resolvedMatch: action.payload,
+        resolvedMatch: payload,
       };
     default:
       return state;
