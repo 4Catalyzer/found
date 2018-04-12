85fa24fe43f19f2c43a923def3a0bf7a30653df5
Kevin Ross <kevin.ross@alienfast.com>
2018-04-12 10:25:29 -0500

    Initial TypeScript declarations and tests (#144)

---
+++ test/typescript/test-usage.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {
+  createBrowserRouter,
+  GetDataArgs,
+  HttpError,
+  makeRouteConfig,
+  Redirect,
+  Route,
+} from 'found';
+import WidgetsPage from './WidgetsPage';
+import AppPage from './AppPage';
+import MainPage from './MainPage';
+
+const fetchWidget = (widgetId: any) =>
+  new Promise(resolve => {
+    throw new Error('oops');
+  });
+
+const fetchWidgets = ({ params, context }: GetDataArgs) => [
+  'foo',
+  'bar',
+  'baz',
+];
+
+const BrowserRouter = createBrowserRouter({
+  routeConfig: makeRouteConfig(
+    <Route path="/" Component={AppPage}>
+      <Route Component={MainPage} />
+      <Route path="widgets">
+        <Route Component={WidgetsPage} getData={fetchWidgets} />
+        <Route
+          path="widgets/:widgetId"
+          getComponent={() =>
+            import('./WidgetsPage').then(module => module.default)
+          }
+          getData={({ params: { widgetId } }) =>
+            fetchWidget(widgetId).catch(() => {
+              throw new HttpError(404);
+            })
+          }
+          render={({ Component, props }) =>
+            Component && props ? (
+              <Component {...props} />
+            ) : (
+              <div>
+                <small>Loading</small>
+              </div>
+            )
+          }
+        />
+      </Route>
+      <Route
+        path="bar"
+        getData={() =>
+          new Promise(resolve => {
+            setTimeout(resolve, 1000, 'Bar');
+          })
+        }
+      />
+      <Redirect from="widget/:widgetId" to="/widgets/:widgetId" />
+    </Route>,
+  ),
+
+  renderError: ({ error }) => (
+    <div>{error.status === 404 ? 'Not found' : 'Error'}</div>
+  ),
+});
+
+ReactDOM.render(<BrowserRouter />, document.getElementById('root'));
