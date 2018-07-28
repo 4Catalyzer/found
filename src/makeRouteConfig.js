e7edf2453367d6dc973974e3d3742e3458c43cc3
Jimmy Jia <tesrin@gmail.com>
2018-07-27 21:11:59 -0400

    Support fragments in makeRouteConfig

--- src/makeRouteConfig.js
+++ src/makeRouteConfig.js
@@ -1,42 +1,60 @@
 import React from 'react';
 
+function buildRouteConfig(node, routeConfig) {
+  React.Children.forEach(node, child => {
+    if (!React.isValidElement(child)) {
+      return;
+    }
+
+    let Type = child.type;
+    const { children, ...props } = child.props;
+
+    if (Type === React.Fragment) {
+      buildRouteConfig(children, routeConfig);
+      return;
+    }
+
+    if (__DEV__) {
+      if (Type.prototype.constructor !== Type) {
+        // Unwrap proxies from react-proxy. This isn't strictly necessary.
+        // eslint-disable-next-line no-param-reassign
+        Type = Type.prototype.constructor;
+      } else if (
+        // eslint-disable-next-line no-underscore-dangle
+        Type.__reactstandin__getCurrent
+      ) {
+        // Unwrap proxies from react-stand-in.
+        // eslint-disable-next-line no-param-reassign
+        Type = Object.getPrototypeOf(Type);
+      }
+    }
+
+    const route = new Type(props);
+
+    if (children) {
+      if (React.isValidElement(children) || Array.isArray(children)) {
+        // eslint-disable-next-line no-use-before-define
+        route.children = makeRouteConfig(children);
+      } else {
+        const routeGroups = {};
+        Object.entries(children).forEach(([groupName, groupRoutes]) => {
+          // eslint-disable-next-line no-use-before-define
+          routeGroups[groupName] = makeRouteConfig(groupRoutes);
+        });
+
+        route.children = routeGroups;
+      }
+    }
+
+    routeConfig.push(route);
+  });
+
+  return routeConfig;
+}
+
 /**
  * Create a route configuration from JSX configuration elements.
  */
 export default function makeRouteConfig(node) {
-  return React.Children.toArray(node)
-    .filter(React.isValidElement)
-    .map(({ type: Type, props: { children, ...props } }) => {
-      if (__DEV__) {
-        if (Type.prototype.constructor !== Type) {
-          // Unwrap proxies from react-proxy. This isn't strictly necessary.
-          // eslint-disable-next-line no-param-reassign
-          Type = Type.prototype.constructor;
-        } else if (
-          // eslint-disable-next-line no-underscore-dangle
-          Type.__reactstandin__getCurrent
-        ) {
-          // Unwrap proxies from react-stand-in.
-          // eslint-disable-next-line no-param-reassign
-          Type = Object.getPrototypeOf(Type);
-        }
-      }
-
-      const route = new Type(props);
-
-      if (children) {
-        if (React.isValidElement(children) || Array.isArray(children)) {
-          route.children = makeRouteConfig(children);
-        } else {
-          const routeGroups = {};
-          Object.entries(children).forEach(([groupName, groupRoutes]) => {
-            routeGroups[groupName] = makeRouteConfig(groupRoutes);
-          });
-
-          route.children = routeGroups;
-        }
-      }
-
-      return route;
-    });
+  return buildRouteConfig(node, []);
 }
