aa4382076289063854b6c7c4f082c18d224e072d
Christopher Pappas <damassi.pappas@gmail.com>
2018-09-09 13:17:38 -0700

    [Lib] Upgrade babel to 7; add docz

---
+++ doczrc.js
@@ -0,0 +1,14 @@
+export default {
+  indexHtml: 'docs/index.html',
+  protocol: 'http',
+  typescript: false,
+  themeConfig: {
+    // Can override any of these colors:
+    // https://github.com/pedronauck/docz/blob/781c2bff5ae6ebe7a0d8a46f9cbc8cfef709c191/packages/docz-theme-default/src/styles/modes.ts#L5
+    colors: {
+      primary: '#6E1EFF',
+      sidebarBg: '#F8F8F8',
+    },
+  },
+  // wrapper: "docs/_utils/components/Wrapper.tsx",
+};
