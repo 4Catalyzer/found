1d0fda4e06e8d15b11569ef365cbc7218a68f943
Jimmy Jia <tesrin@gmail.com>
2018-12-29 10:52:49 -0800

    fix: Fix withRouter (#221)

--- test/ElementsRenderer.test.js
+++ test/ElementsRenderer.test.js
@@ -4,25 +4,23 @@ import React from 'react';
 import ElementsRenderer from '../src/ElementsRenderer';
 
 describe('<ElementsRenderer>', () => {
-  it('should render null when no item in elements', () => {
-    const elements = [];
-    const wrapper = mount(<ElementsRenderer elements={elements} />);
+  it('should render null when there are no elements', () => {
+    const wrapper = mount(<ElementsRenderer elements={[]} />);
     expect(wrapper.html()).toBe(null);
   });
 
-  it('should render element', () => {
-    const elements = [<div />];
-    const wrapper = mount(<ElementsRenderer elements={elements} />);
+  it('should render a single element', () => {
+    const wrapper = mount(<ElementsRenderer elements={[<div />]} />);
     expect(wrapper.find('div')).toHaveLength(1);
   });
 
-  it('should render elements with nested structure', () => {
+  it('should render nested elements', () => {
     const Parent = ({ children }) => <div className="parent">{children}</div>;
     const Child = () => <div className="child" />;
 
-    const elements = [<Parent />, <Child />];
-
-    const wrapper = mount(<ElementsRenderer elements={elements} />);
+    const wrapper = mount(
+      <ElementsRenderer elements={[<Parent />, <Child />]} />,
+    );
 
     const parent = wrapper.find(Parent);
     expect(parent).toHaveLength(1);
