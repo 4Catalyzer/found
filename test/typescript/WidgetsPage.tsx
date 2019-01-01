f1e94efb2908f9c800691d67aecae596e4a851a2
Kevin Ross <kevin.ross@alienfast.com>
2019-01-01 12:29:53 -0600

    include original typescript tests

--- test/typescript/WidgetsPage.tsx
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-
-const Component = () => <div />;
-export default Component;
---
+++ types/example-app/WidgetsPage.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+
+const WidgetsPage = () => <div />;
+export { WidgetsPage };
