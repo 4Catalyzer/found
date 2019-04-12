3093fb07a9194bc996ab4b1b5ed939f34903dea4
Jimmy Jia <tesrin@gmail.com>
2019-04-12 11:44:12 -0400

    fix: Define type for useRouter (#334)

---
+++ types/example-app/ComponentUsingRouter.tsx
@@ -0,0 +1,18 @@
+import useRouter from 'found/lib/useRouter';
+import * as React from 'react';
+
+interface Props {
+  foo?: boolean;
+}
+
+function ComponentUsingRouter(_props: Props) {
+  const { match, router } = useRouter();
+
+  return (
+    <div>
+      {match.location.pathname}, {router}
+    </div>
+  );
+}
+
+export { ComponentUsingRouter };
