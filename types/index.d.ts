952bec3df9dc23afa0a71e8b0317d8d93f2f6918
Jimmy Jia <tesrin@gmail.com>
2019-07-17 09:31:20 -0400

    nit

--- types/index.d.ts
+++ types/index.d.ts
@@ -320,9 +320,9 @@ declare module 'found' {
   }
 
   class HttpError {
+    constructor(status: number, data?: any);
     status: number;
     data: any;
-    constructor(status: number, data?: any);
   }
 
   interface RedirectProps {
@@ -367,8 +367,8 @@ declare module 'found' {
   ): React.ComponentType<Omit<Props, keyof RouterState>>;
 
   class RedirectException {
-    location: LocationDescriptor;
     constructor(location: LocationDescriptor);
+    location: LocationDescriptor;
   }
 
   /**
@@ -436,8 +436,8 @@ declare module 'found' {
 
   // Improve these `any`s as needed
   type ConnectedRouter = React.ComponentType<{
-    resolver: Resolver;
     matchContext?: any;
+    resolver: Resolver;
     initialRenderArgs?: RouterRenderArgs;
   }>;
 
