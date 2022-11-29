f8dfda893e9fd04c6ecdacc526a8cc291447e896
Szymon Wiszczuk <szymon.wiszczuk@nordsec.com>
2022-11-30 00:09:15 +0100

    minor changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,9 @@ module.exports = {
   plugins: ['prettier', 'import'],
   parser: '@typescript-eslint/parser',
   settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
     'import/resolver': {
       typescript: {
-        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        alwaysTryTypes: true,
       },
     },
   },
