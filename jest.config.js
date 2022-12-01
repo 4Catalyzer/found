38c80eab0fc670aed799fdd3af4dee94fb088c50
Szymon Wiszczuk <golota60@gmail.com>
2022-12-01 19:50:43 +0100

    chore: setup tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  preset: 'ts-jest/presets/js-with-ts', // TODO: change to ts-jest once all repo is converted
   testEnvironment: 'jsdom',
   collectCoverageFrom: ['src/**'],
   resetMocks: true,
   restoreMocks: true,
   setupFiles: ['<rootDir>/test/setup.ts'],
   testPathIgnorePatterns: ['/node_modules/', '<rootDir>/types/'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.json' }],
+  },
 };
