718ba148b8a75c38baeb364d981f540a0f4ded4d
jquense <monastic.panic@gmail.com>
2025-04-02 13:28:11 -0400

    fix: include pathToRegexp

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 
 echo "Build library"
-yarn babel src --out-dir lib --delete-dir-on-start --extensions .ts,.tsx --ignore '**/*.d.ts'
-yarn babel src --out-dir cjs --env-name cjs --delete-dir-on-start --extensions .ts,.tsx --ignore '**/*.d.ts'  
+yarn babel src --out-dir lib --delete-dir-on-start --extensions .ts,.tsx --ignore '**/*.d.ts'  --copy-files --no-copy-ignored
+yarn babel src --out-dir cjs --env-name cjs --delete-dir-on-start --extensions .ts,.tsx --ignore '**/*.d.ts' --copy-files --no-copy-ignored
 
 
 echo "{ \"type\": \"commonjs\" }" > ./cjs/package.json
